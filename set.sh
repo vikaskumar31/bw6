79ee2bf29957d88235f7f15334a77c6f9c75a421
Vikasaadu <vikas.aadu@gmail.com>
2017-11-14 17:38:30 +0100

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-sudo apt-get update
-sudo apt-get install nfs-common
-sudo su
-echo "centralrepo.localdomain:/opt/scripts /opt/scripts nfs   defaults        0       0" >> /etc/fstab
-sudo su tibco
-sudo mkdir /opt/installers
-sudo mkdir /opt/scripts
-chown -r tibco:tibco /opt/installers
-chown -r tibco:tibco /opt/scripts
-sudo mount -t nfs centralrepo:/opt/installers /opt/installers
-sudo mount -t nfs centralrepo:/opt/scripts /opt/scripts
+sudo mkdir /opt/scripts12
